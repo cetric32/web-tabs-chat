66343f79ac39455bb6ca01a8696266b593e36147
cetric Lihalakha <cetric32@gmail.com>
2022-09-21 07:36:11 +0300

    add saving to session storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 //import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { connect } from "react-redux";
 import { useEffect, useState } from "react";
 import { Channel } from "./common/functions";
 import { storeChats, storeOwnChats, updateProfile } from "./store/actions";
 import { v4 as uuidv4 } from "uuid";
 import _ from "lodash";
-import { Button, Form, ListGroup } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import dayjs from "dayjs";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 function App(props) {
   const [message, setMessage] = useState("");
@@ -14,86 +23,159 @@ function App(props) {
 
   useEffect(() => {
     Channel.addEventListener("message", (event) => {
-      //console.log("message event", event.data);
       props.storeChats({ message: event.data });
     });
   });
 
-  console.log("====================================");
-  console.log("props.chats", props.chats);
-  console.log("====================================");
+  useEffect(() => {
+    if (name) {
+      document.title = `Web Chat | ${name}`;
+    }
+  }, [name]);
 
   if (_.get(props, "profile.name", "")) {
     return (
-      <Container>
-        <header className="App-header">
-          <h1>Welcome {_.get(props, "profile.name", "")}</h1>
-          <ListGroup>
-            {props.chats.map((msg) => {
-              return (
-                <ListGroup.Item key={msg.key}>
-                  {msg.name}: {msg.message}
-                </ListGroup.Item>
-              );
-            })}
-          </ListGroup>
-          <p>
-            <form
-              onSubmit={(event) => {
-                event.preventDefault();
+      <Container style={{ height: "90%" }}>
+        <h1 className="" style={{ float: "right" }}>
+          We Chat({_.get(props, "profile.name", "")})
+        </h1>
 
-                const message1 = {
-                  message: message,
-                  name: _.get(props, "profile.name", ""),
-                  key: uuidv4(),
-                  date: new Date().toISOString(),
-                };
+        {props.chats.map((msg) => {
+          return (
+            <Row key={msg.key} style={{ width: "100%", marginBottom: 15 }}>
+              <Col xs={6}>
+                {msg.name === _.get(props, "profile.name", "") ? (
+                  ""
+                ) : (
+                  <>
+                    <Form.Text
+                      className="text-muted"
+                      style={{ float: "right" }}
+                    >
+                      {msg.name}
+                    </Form.Text>
+                    <Card
+                      style={{
+                        width: "100%",
+                        background: "#E5E5EA",
+                        color: "black",
+                        float: "left",
+                        clear: "both",
+                        borderBottomLeftRadius: " 30px 0px9",
+                      }}
+                    >
+                      <Card.Body>
+                        <Card.Text>{`${msg.message}`}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                    <Form.Text className="text-muted">
+                      {dayjs(msg.date).fromNow()}
+                    </Form.Text>
+                  </>
+                )}
+              </Col>
+              <Col xs={6}>
+                {msg.name !== _.get(props, "profile.name", "") ? (
+                  ""
+                ) : (
+                  <>
+                    <Card
+                      style={{
+                        width: "100%",
+                        background: " #00e34d",
+                        color: "white",
+                        float: "right",
+                        clear: "both",
+                        borderBottomRightRadius: "20px 0px9",
+                      }}
+                      m-1
+                    >
+                      <Card.Body>
+                        <Card.Text>{`${msg.message}`}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                    <Form.Text className="text-muted">
+                      {dayjs(msg.date).fromNow()}
+                    </Form.Text>
+                  </>
+                )}
+              </Col>
+            </Row>
+          );
+        })}
 
-                Channel.postMessage(message1);
-                props.storeOwnChats({ message: message1 });
-                setMessage("");
+        <div class="fixed-bottom w-75 mx-auto">
+          <Form
+            onSubmit={(event) => {
+              event.preventDefault();
+
+              const message1 = {
+                message: message,
+                name: _.get(props, "profile.name", ""),
+                key: uuidv4(),
+                date: new Date().toISOString(),
+              };
+
+              Channel.postMessage(message1);
+              props.storeOwnChats({ message: message1 });
+              setMessage("");
+            }}
+          >
+            <Form.Control
+              type="text"
+              placeholder="message..."
+              required
+              value={message}
+              onChange={(event) => {
+                setMessage(event.target.value);
               }}
-            >
-              <input
-                type="text"
-                value={message}
-                onChange={(event) => {
-                  setMessage(event.target.value);
-                }}
-              />
-              <input type="submit" value="Send Message" />
-            </form>
-          </p>
-        </header>
+              size="md"
+              className={"m-2 "}
+            />
+            <Button variant="primary" type="submit">
+              Send Message
+            </Button>
+          </Form>
+        </div>
       </Container>
     );
   } else {
     return (
-      <Container>
-        <h1>Login to your account</h1>
-        <Form
-          onSubmit={(event) => {
-            event.preventDefault();
-            props.updateProfile({ name });
-            setName("");
-          }}
-        >
-          <Form.Group className="mb-3">
-            <Form.Label>Your Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Your name"
-              required
-              value={name}
-              onChange={(event) => {
-                setName(event.target.value);
+      <Container className="w-100 h-100">
+        <Row className="d-flex justify-content-center align-items-center h-100 my-auto">
+          <Col md={6}>
+            <h1
+              className="text-right 1-100"
+              style={{ float: "right", position: "relative" }}
+            >
+              Web Chat
+            </h1>
+            <h3 className="mt-5">Login to your account</h3>
+            <Form
+              onSubmit={(event) => {
+                event.preventDefault();
+                props.updateProfile({ name });
+                setName("");
               }}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
+            >
+              <Form.Group className="mb-3">
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Your name"
+                  required
+                  value={name}
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     );
   }
--- src/store/reducers/chats-reducer.js
+++ src/store/reducers/chats-reducer.js
@@ -1,3 +1,4 @@
+import { readFromStorage, saveToStorage } from "../../common/functions";
 import {
   POST_MESSAGE,
   POST_MESSAGE_SUCCESSFUL,
@@ -5,47 +6,66 @@ import {
   SET_PROFILE_SUCCESSFUL,
 } from "../action-const";
 
-const initialState = {
-  chats: [],
-  profile: {
-    name: "",
-    time: new Date().toUTCString(),
-  },
-  myChats: [],
-};
+let myStoredState = readFromStorage("myState");
+
+console.log("====================================");
+console.log("myStoredState", myStoredState);
+console.log("====================================");
+
+const initialState = myStoredState
+  ? myStoredState
+  : {
+      chats: [],
+      profile: {
+        name: "",
+        time: new Date().toUTCString(),
+      },
+      myChats: [],
+    };
 
 export default function chatsReducer(state = initialState, action) {
+  let newState = {};
   switch (action.type) {
     case POST_MESSAGE_SUCCESSFUL:
       const existingObj = state.chats.find((c) => {
         return c.key === action.payload.key;
       });
 
-      console.log("existingObj", existingObj);
-
       if (existingObj) {
         return state;
       } else {
-        return {
+        newState = {
           ...state,
           chats: state.chats.concat(action.payload),
         };
+
+        saveToStorage("myState", newState);
+
+        return newState;
       }
 
     case POST_OWN_MESSAGE_SUCCESSFUL:
-      return {
+      newState = {
         ...state,
         myChats: state.myChats.concat(action.payload),
       };
 
+      saveToStorage("myState", newState);
+
+      return newState;
+
     case SET_PROFILE_SUCCESSFUL:
-      return {
+      newState = {
         ...state,
         profile: {
           ...state.profile,
           name: action.payload,
         },
       };
+
+      saveToStorage("myState", newState);
+
+      return newState;
 
     default:
       return state;
